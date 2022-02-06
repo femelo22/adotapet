f9e47272ec135536e6b674851be5d6b80a7c2f04
luiz fernando <luiz123jfmg@gmail.com>
2022-02-06 19:14:30 -0300

    Configurações iniciais do projeto

---
+++ src/main/kotlin/lf/adotapet/AdotapetApplication.kt
@@ -0,0 +1,11 @@
+package lf.adotapet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdotapetApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdotapetApplication>(*args)
+}
---
+++ src/test/kotlin/lf/adotapet/AdotapetApplicationTests.kt
@@ -0,0 +1,13 @@
+package lf.adotapet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdotapetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
